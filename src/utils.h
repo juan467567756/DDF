d95c88f3b65832fee3e76b353a3cab701c0a7172
Juan Salvador Sleibe <Juansleibe2004@gmail.com>
2025-05-04 21:33:42 -0300

    V0.2

---
+++ src/utils.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+void box(const std::string& text);        // bloque rectangular
+void arrow();                             // flecha vertical
+void decision(const std::string& question); // bloque de decisión SÍ/NO
+
