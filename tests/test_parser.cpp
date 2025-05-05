b682d5d18d5f1e886ae10e8e46d049431107bbdb
Juan Salvador Sleibe <Juansleibe2004@gmail.com>
2025-05-05 12:36:47 -0300

    v2.2: tests reales de parser con validaciones de AST y errores

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -1,18 +1,44 @@
-#include "../src/parser.hpp"   
+#include "../src/parser.hpp"
 #include <gtest/gtest.h>
+#include <sstream>
 
-// Cada TEST describe un caso de la gramática
-TEST(ParserTest, EmptyProgram) {
-  // TODO: invocar el parser con "" y esperar programa vacío
-  SUCCEED();
+AST parseStr(const std::string& input) {
+    std::istringstream iss(input);
+    return parseProgram(iss);
 }
 
-TEST(ParserTest, SingleBox) {
-  // TODO: invocar parser("BOX Hola") y comprobar AST con un BoxNode("Hola")
-  SUCCEED();
+TEST(ParserTest, ParseBoxAndArrow) {
+    AST ast = parseStr("BOX Inicio\nARROW\nBOX Fin");
+    ASSERT_EQ(ast.size(), 3);
+    EXPECT_NE(dynamic_cast<BoxNode*>(ast[0].get()), nullptr);
+    EXPECT_NE(dynamic_cast<ArrowNode*>(ast[1].get()), nullptr);
+    EXPECT_NE(dynamic_cast<BoxNode*>(ast[2].get()), nullptr);
 }
 
-TEST(ParserTest, DecisionElseLoopErrors) {
-  // TODO: cada error sintáctico debe lanzar ParserError con línea y razón
-  SUCCEED();
+TEST(ParserTest, ParseDecisionAndElse) {
+    AST ast = parseStr("DECIDE X > 0\nBOX Positivo\nELSE\nBOX Negativo");
+    ASSERT_EQ(ast.size(), 4);
+    EXPECT_NE(dynamic_cast<DecisionNode*>(ast[0].get()), nullptr);
+    EXPECT_NE(dynamic_cast<BoxNode*>(ast[1].get()), nullptr);
+    EXPECT_NE(dynamic_cast<ElseNode*>(ast[2].get()), nullptr);
+    EXPECT_NE(dynamic_cast<BoxNode*>(ast[3].get()), nullptr);
 }
+
+TEST(ParserTest, ParseLoop) {
+    AST ast = parseStr("LOOP repetir\nBOX Acción\nENDLOOP");
+    ASSERT_EQ(ast.size(), 3);
+    EXPECT_NE(dynamic_cast<LoopNode*>(ast[0].get()), nullptr);
+    EXPECT_NE(dynamic_cast<BoxNode*>(ast[1].get()), nullptr);
+    EXPECT_NE(dynamic_cast<EndLoopNode*>(ast[2].get()), nullptr);
+}
+
+TEST(ParserTest, ParseInvalidKeyword) {
+    EXPECT_THROW(parseStr("FOOBAR"), ParserError);
+}
+
+TEST(ParserTest, ParseMissingText) {
+    EXPECT_THROW(parseStr("BOX"), ParserError);
+    EXPECT_THROW(parseStr("DECIDE"), ParserError);
+    EXPECT_THROW(parseStr("LOOP"), ParserError);
+}
+
