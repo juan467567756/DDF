46d154a5a2c6d917b51eb3300db6b4a7dfd5676b
Juan Salvador Sleibe <Juansleibe2004@gmail.com>
2025-05-05 10:24:33 -0300

    test: añade CMakeLists.txt para GoogleTest

---
+++ tests/test_utils.cpp
@@ -0,0 +1,18 @@
+#include "../src/utils.h"
+#include <gtest/gtest.h>
+#include <sstream>
+
+// Captura la salida de box()
+std::string captureBox(const std::string& text) {
+    std::ostringstream ss;
+    auto* old = std::cout.rdbuf(ss.rdbuf());
+    box(text);
+    std::cout.rdbuf(old);
+    return ss.str();
+}
+
+TEST(UtilsTest, BoxFramesText) {
+    std::string out = captureBox("Test");
+    EXPECT_NE(out.find("+-----+"), std::string::npos);
+    EXPECT_NE(out.find("| Test |"), std::string::npos);
+}
